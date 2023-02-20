f1338ee1043a08cb7c05073c53328146f0fcc14c
gavfu <gavfu@outlook.com>
2023-02-20 13:56:21 +0800

    Update name and symbol

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,7 +2,7 @@ import { ethers, upgrades } from "hardhat";
 
 async function main() {
   const Carbon3Label = await ethers.getContractFactory("Carbon3Label");
-  const contract = await upgrades.deployProxy(Carbon3Label, ["Carbon3Label", "C3L"]);
+  const contract = await upgrades.deployProxy(Carbon3Label, ["Automotive Carbon3 Trust Label", "PCFLabel"]);
   console.log(`Deployed Carbon3Label to ${contract.address}`);
 }
 
